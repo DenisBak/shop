c32bc7963e94e6dc615f361ba70f075ae417fc0f
Denis Baklanov <12baklanovsamara08@gmail.com>
2024-01-27 23:36:12 +0400

    Initial commit, create: 1. Liquibase scripts for shop_db 2. Docker-compose for whole project 3. Basic auth for admin user

--- src/main/java/ru/multicard/mock/shop/controller/ShopController.java
+++ src/main/java/ru/multicard/mock/shop/controller/ShopController.java
@@ -1,12 +1,9 @@
 package ru.multicard.mock.shop.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.multicard.mock.shop.controller.response.ClientDto;
-import ru.multicard.mock.shop.service.ClientService;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import ru.multicard.mock.shop.service.ClientService;
 
 @RestController
 @RequiredArgsConstructor
